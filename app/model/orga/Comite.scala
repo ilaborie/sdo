a6e2847f2d9c3e87781a92e3c08832cbd6a71077
Igor Laborie <ilaborie@gmail.com>
2013-07-26 15:24:24 +0200

    Add classement and club menu

--- app/model/orga/Comite.scala
+++ app/model/orga/Comite.scala
@@ -5,13 +5,14 @@ package model.orga
  * Date: 21/07/13
  * Time: 11:19
  */
-case class Comite(name: String, shortName: String, info: Option[String]) {
+case class Comite(name: String, shortName: String, info: Option[Info]) {
   lazy val fullName = s"[$shortName] $name"
 
   override def toString = fullName
 }
 
 object Comite {
-  val ToulouseNord = Comite("Toulouse Nord", "TLS", None)
-  val Toulouse = Comite("Toulouse", "NTL", None)
+
+  val ToulouseNord = Comite("Toulouse Nord", "NTL", None)
+  val Toulouse = Comite("Toulouse", "TLS", None)
 }
--- app/model/orga/Ligue.scala
+++ app/model/orga/Ligue.scala
@@ -5,16 +5,17 @@ package model.orga
  * Date: 21/07/13
  * Time: 11:19
  */
-case class Ligue(name: String, shortName: String, comites: Set[Comite], info: Option[String]) {
+case class Ligue(name: String, shortName: String, comites: Seq[Comite], info: Option[Info]) {
   lazy val fullName = s"[$shortName] $name"
 
   override def toString = fullName
 }
 
 object Ligue {
-  val SudOuest = Ligue("Sud Ouest", "SDO", Set(Comite.Toulouse, Comite.ToulouseNord), None)
 
-  val all: Seq[Ligue] = List(SudOuest)
+  val all: Seq[Ligue] = List(
+    Ligue("Sud Ouest", "SDO", List(Comite.Toulouse, Comite.ToulouseNord), None)
+  )
 
   def findByShortName(shortName: String): Option[Ligue] = all.find(_.shortName == shortName)
 }
