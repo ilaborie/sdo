06c28ce1c55a81f7a4defd73d4bf587c90bc6209
Igor Laborie <ilaborie@gmail.com>
2013-07-26 17:29:59 +0200

    Ranking Team basic

--- app/model/orga/Club.scala
+++ app/model/orga/Club.scala
@@ -10,17 +10,30 @@ case class Club(name: String, shortName: String, comite: Comite, info: Option[In
 }
 
 object Club {
+  val Rocco = Club("Rocco 6 fléches", "Rocco", Comite.Toulouse, None)
+  val DDD = Club("Dubliners Darts Drinkers", "DDD", Comite.Toulouse, None)
+  val X1 = Club("Double As", "X1", Comite.Toulouse, None)
+  val Wood = Club("Woodpecker", "Wood", Comite.Toulouse, None)
+  val Coch = Club("Cochonou's", "Coch.", Comite.Toulouse, None)
+  val FT = Club("Fléches Tordues", "FT", Comite.ToulouseNord, None)
+  val ONeill = Club("O'Neill", "ONeill", Comite.ToulouseNord, None)
+  val DCMAG = Club("Darts club Mérignac Arlac Gironde", "DC MAG", Comite.ToulouseNord, None)
+
   val all = List(
-    Club("Rocco 6 fléches", "Rocco", Comite.Toulouse, None),
-    Club("Dubliners Darts Drinkers", "Dub", Comite.Toulouse, None),
-    Club("Double As", "X1", Comite.Toulouse, None),
-    Club("Woodpecker", "Wood", Comite.Toulouse, None),
-    Club("Cochonou's", "Coch.", Comite.Toulouse, None),
-    Club("Double As", "X1", Comite.Toulouse, None),
-    Club("Fléches Tordues", "FT", Comite.ToulouseNord, None),
-    Club("O'Neill", "ONeill", Comite.ToulouseNord, None),
-    Club("Darts club Mérignac Arlac Gironde", "DC MAG", Comite.ToulouseNord, None)
+    Rocco,
+    DDD,
+    X1,
+    Wood,
+    Coch,
+    FT,
+    ONeill,
+    DCMAG
   )
 
   def findByComite(comite: Comite) = all.filter(_.comite == comite)
+
+  def findByShortName(shortName: String): Option[Club] = all.filter(_.shortName == shortName) match {
+    case club :: _ => Some(club)
+    case Nil => None
+  }
 }
---
+++ app/model/rank/Rank.scala
@@ -0,0 +1,40 @@
+package model.rank
+
+import model.orga._
+
+case class SeasonRanking(season: Season, teamRanks: Seq[TeamRank]) {
+  lazy val ordered: Seq[TeamRank] = teamRanks.sortBy(getPosition)
+
+  def getPosition(teamRank: TeamRank): Int = {
+    // FIXME Cache data
+    def betterThan(other: TeamRank): Boolean = (other.points > teamRank.points) || (
+      (other.points == teamRank.points) && (other.diff > teamRank.diff))
+    1 + teamRanks.count(betterThan)
+  }
+}
+
+object SeasonRanking {
+  // FIXME dummy data
+
+  private val list = List(
+    TeamRank(Team.Satanas, win = 9, loose = 0, draw = 0, plus = 133, minus = 47),
+    TeamRank(Team.DDD, win = 7, loose = 1, draw = 1, plus = 127, minus = 53),
+    TeamRank(Team.X1, win = 7, loose = 2, draw = 0, plus = 119, minus = 61),
+    TeamRank(Team.ONeill, win = 5, loose = 3, draw = 1, plus = 93, minus = 58),
+    TeamRank(Team.FT, win = 4, loose = 5, draw = 0, plus = 85, minus = 95),
+    TeamRank(Team.Coch, win = 2, loose = 5, draw = 1, fail = 1, plus = 62, minus = 98),
+    TeamRank(Team.Diabolo, win = 1, loose = 5, draw = 1, fail = 2, plus = 47, minus = 93),
+    TeamRank(Team.Wood, win = 1, loose = 5, draw = 1, fail = 1, plus = 55, minus = 105)
+  )
+
+
+  def forComite(comite: Comite) = SeasonRanking("2012-2013", list.filter(_.team.club.comite == comite))
+
+}
+
+case class TeamRank(team: Team, win: Int, loose: Int, draw: Int, fail: Int = 0, plus: Int, minus: Int) {
+  val days: Int = win + loose + draw + fail
+  val points: Int = 3 * win + draw
+  val diff: Int = plus - minus
+
+}
