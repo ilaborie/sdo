5c069d1ef5088451bb7766a5917921fa9fa0f3df
Igor Laborie <ilaborie@gmail.com>
2013-07-31 00:15:43 +0200

    Support Team omition

--- app/model/orga/Data.scala
+++ app/model/orga/Data.scala
@@ -130,7 +130,9 @@ object Data {
     val name = info("name").asInstanceOf[String]
     val playerList = info("players").asInstanceOf[JavaList[JavaMap[String, String]]].toList
     val players = for (player <- playerList) yield createLicensedPlayer(player.toMap)
-    Team(name, players)
+    val omit = info.contains("omit")
+
+    Team(name, players, omit)
   }
 
   /**
