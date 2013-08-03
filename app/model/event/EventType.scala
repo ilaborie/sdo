929038dffe514ae02c354024b622b09e78e42640
Igor Laborie <ilaborie@gmail.com>
2013-08-03 19:37:18 +0200

    Update Events (styling)

---
+++ app/model/event/EventType.scala
@@ -0,0 +1,55 @@
+package model.event
+
+/**
+  */
+sealed abstract class EventType {
+  def style: String
+}
+
+object EventType {
+  def apply(evtType: String): EventType = {
+    evtType match {
+      case "world france" => WorldFranceEvent
+      case "world" => WorldEvent
+      case "france" => FranceEvent
+      case "fede" => FederationEvent
+      case "ligue" => LigueEvent
+      case "comite" => ComiteEvent
+      case _ => DefaultEvent
+    }
+  }
+}
+
+
+object WorldEvent extends EventType {
+  val style = "world"
+}
+
+object WorldFranceEvent extends EventType {
+  val style = "worldfrance"
+}
+
+object FranceEvent extends EventType {
+  val style = "france"
+}
+
+object FederationEvent extends EventType {
+  val style = "fede"
+}
+
+object LigueEvent extends EventType {
+  val style = "ligue"
+}
+
+object ComiteEvent extends EventType {
+  val style = "comite"
+}
+
+object TeamEvent extends EventType {
+  val style = "team"
+}
+
+
+object DefaultEvent extends EventType {
+  val style = "default"
+}
