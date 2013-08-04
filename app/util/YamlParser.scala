d03cc255bb9954c6330a71454832ead65683342c
Igor Laborie <ilaborie@gmail.com>
2013-08-04 13:13:39 +0200

    FIX Use Guava

--- app/util/YamlParser.scala
+++ app/util/YamlParser.scala
@@ -1,8 +1,7 @@
 package util
 
 import play.Play
-import java.io.InputStreamReader
-import com.google.common.io.CharStreams
+import com.google.common.io.Files
 import com.google.common.base.Charsets
 import play.api.Logger
 import org.yaml.snakeyaml.Yaml
@@ -17,20 +16,17 @@ case class YamlParser(app: Application) {
   private val logger = Logger("YAML")
   private val yaml = new Yaml(new CustomClassLoaderConstructor(Play.application.classloader))
 
-  def parseFile(path: String): Any = app.resourceAsStream(path) match {
+  def parseFile(path: String): Any = app.getExistingFile("conf/" + path) match {
     case None => throw new IllegalArgumentException(s"File $path not found")
-    case Some(stream) => {
-      val reader = new InputStreamReader(stream, Charsets.UTF_8)
+    case Some(file) => {
       try {
-        val data = CharStreams.toString(reader)
+        val data = Files.toString(file, Charsets.UTF_8)
         logger.trace(s"$path content: $data")
         val parsed = yaml.load(data)
         logger.debug(s"parsed: $parsed")
         parsed
       } catch {
         case t: Throwable => new IllegalStateException(s"Fail to parse $path", t)
-      } finally {
-        reader.close()
       }
     }
   }
