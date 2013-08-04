9637bed2a2f63ba98a5706dbdbefeea67585fced
Igor Laborie <ilaborie@gmail.com>
2013-08-04 13:27:05 +0200

    Fix file path

--- app/util/YamlParser.scala
+++ app/util/YamlParser.scala
@@ -7,6 +7,7 @@ import play.api.Logger
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.CustomClassLoaderConstructor
 import play.api.Application
+import java.io.File
 
 /**
  * A YAML Parser
@@ -16,26 +17,31 @@ case class YamlParser(app: Application) {
   private val logger = Logger("YAML")
   private val yaml = new Yaml(new CustomClassLoaderConstructor(Play.application.classloader))
 
-  def parseFile(path: String): Any = app.getExistingFile("conf/" + path) match {
-    case None => throw new IllegalArgumentException(s"File $path not found")
-    case Some(file) => {
-      try {
-        val data = Files.toString(file, Charsets.UTF_8)
-        logger.trace(s"$path content: $data")
-        val parsed = yaml.load(data)
-        logger.debug(s"parsed: $parsed")
-        parsed
-      } catch {
-        case t: Throwable => new IllegalStateException(s"Fail to parse $path", t)
-      }
+  def parseFile(file: File): Any =
+    try {
+      val data = Files.toString(file, Charsets.UTF_8)
+      logger.trace(s"$file content: $data")
+      val parsed = yaml.load(data)
+      logger.debug(s"parsed: $parsed")
+      parsed
+    } catch {
+      case t: Throwable => throw new IllegalStateException(s"Fail to parse $file", t)
     }
-  }
+
 }
 
 object YamlParser {
   var parser: YamlParser = null
 
-  def parseFile(path: String): Any = parser.parseFile(path)
+  def parseFile(path: String): Any = {
+    val parent = new File(parser.app.configuration.getString("file.data").getOrElse("conf"))
+    val file = new File(parent.getAbsoluteFile + File.separator + path)
+    //if (file.exists && parent.isFile) {
+      parser.parseFile(file)
+    //} else {
+    //  throw new IllegalArgumentException(s"File ${file.getAbsolutePath} not found")
+    //}
+  }
 
   def tryParseFile(path: String): Option[Any] = try {
     Some(parseFile(path))
