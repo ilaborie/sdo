a25ca54368cadae6fef93058aacf1c3b3cba5546
Igor Laborie <ilaborie@gmail.com>
2013-08-09 11:36:25 +0200

    Update build

--- project/Build.scala
+++ project/Build.scala
@@ -8,21 +8,25 @@ object ApplicationBuild extends Build {
   val appName = "sdo"
   val appVersion = "1.0-SNAPSHOT"
 
+  val secureSocial = "securesocial" %% "securesocial" % "master-SNAPSHOT"
+
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    secureSocial
   )
 
   // Only compile main.less
-  def customLessEntryPoints(base: File): PathFinder = ((base / "app" / "assets" / "style" * "*.less"))
+  def customLessEntryPoints(base: File): PathFinder = base / "app" / "assets" / "style" * "*.less"
 
 
   val defaultSettings =  Project.defaultSettings ++ org.scalastyle.sbt.ScalastylePlugin.Settings
 
   val main = play.Project(appName, appVersion, appDependencies, settings= defaultSettings).settings(
     // Add your own project settings here
-    lessEntryPoints <<= baseDirectory(customLessEntryPoints)
+    lessEntryPoints <<= baseDirectory(customLessEntryPoints),
+    resolvers += Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
   )
 
 
