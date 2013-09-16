b48395e18072df194b018fa72884abce2e91566e
Igor Laborie <ilaborie@gmail.com>
2013-09-16 23:35:00 +0200

    iCalendar support

--- app/controllers/Event.scala
+++ app/controllers/Event.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play.api.mvc._
 import model.event.EventYear
+import org.joda.time.LocalDate
 
 /**
  * Mains pages
@@ -23,6 +24,18 @@ object Event extends Controller {
   def eventsList() = Action {
     Ok(views.html.event.list(model.event.Event.events))
   }
+
+
+  /**
+   * Events List
+   * @return list page
+   */
+  def icalendar() = Action {
+    val today = LocalDate.now
+    val events = model.event.Event.events.filter(_.to.isAfter(today))
+    Ok(views.html.event.icalendar(events)).withHeaders("Content-Type" -> "text/calendar; charset=UTF-8")
+  }
+
 
   /**
    * Events calendar
